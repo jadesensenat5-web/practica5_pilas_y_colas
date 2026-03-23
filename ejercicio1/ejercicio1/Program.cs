818a97caee9eb11fd8ad0716dedf0f0f3b4f4f0f
jadesensenat5-web <jadesensenat5@gmail.com>
2026-03-23 06:58:25 -0400

    ejercicio1

---
+++ ejercicio1/ejercicio1/Program.cs
@@ -0,0 +1,22 @@
+﻿// 1. Crear una pila y agregar 4 numeros.
+
+using System;
+using System.Collections.Generic;
+
+class Ejercicio1
+{
+    static void Main()
+    {
+        Stack<int> pila = new Stack<int>();
+        pila.Push(10);
+        pila.Push(20);
+        pila.Push(30);
+        pila.Push(40);
+
+        Console.WriteLine("El contenido de la pila (del último al primero):");
+        foreach (int n in pila)
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
