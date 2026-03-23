2347df318ed10e6b64d0738121b2235c30cd78a0
jadesensenat5-web <jadesensenat5@gmail.com>
2026-03-23 08:01:34 -0400

    ejercicio5

---
+++ ejercicio5/ejercicio5/Program.cs
@@ -0,0 +1,33 @@
+﻿// 5. Sumar elementos de una cola.
+using System;
+using System.Collections.Generic;
+
+class Ejercicio5
+{
+    static void Main()
+    {
+        Queue<int> miCola = new Queue<int>();
+        miCola.Enqueue(10);
+        miCola.Enqueue(20);
+        miCola.Enqueue(30);
+        miCola.Enqueue(40);
+        miCola.Enqueue(50);
+        Console.WriteLine("Calculando la suma de los elementos en la cola...");
+        Console.WriteLine("Elementos: 10, 20, 30, 40, 50");
+        int total = SumarCola(miCola);
+        Console.WriteLine("-------------------------------------------");
+        Console.WriteLine("EL TOTAL DE LA SUMA ES: " + total);
+        Console.WriteLine("-------------------------------------------");
+        Console.WriteLine("\nPresiona ENTER para cerrar...");
+        Console.ReadLine();
+    }
+    static int SumarCola(Queue<int> c)
+    {
+        int suma = 0;
+        foreach (int numero in c)
+        {
+            suma += numero;
+        }
+        return suma;
+    }
+}
