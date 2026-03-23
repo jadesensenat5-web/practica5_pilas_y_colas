a1da162518ca2f4667e6e7f19f74fe7d289d63bf
jadesensenat5-web <jadesensenat5@gmail.com>
2026-03-23 07:12:14 -0400

    ejercicio2

---
+++ ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,25 @@
+﻿// 2. Recorrer y Eliminar todos los elementos.
+
+using System;
+using System.Collections.Generic;
+
+class Ejercicio2
+{
+    static void Main()
+    {
+        Stack<int> pila = new Stack<int>();
+        pila.Push(1);
+        pila.Push(2);
+        pila.Push(3);
+        pila.Push(4);
+        Console.WriteLine("Iniciando vaciado de la pila...");
+        while (pila.Count > 0)
+        {
+            int n = pila.Pop();
+            Console.WriteLine("Eliminé el número: " + n);
+        }
+        Console.WriteLine("\n¿Cuántos elementos quedan? " + pila.Count);
+        Console.WriteLine("La pila está vacía ahora.");
+        Console.ReadLine();
+    }
+}
