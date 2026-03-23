0c96cffd4a907334c3f2b2b1bbaa77cd078fe38d
jadesensenat5-web <jadesensenat5@gmail.com>
2026-03-23 07:47:29 -0400

    ejercicio3

---
+++ ejercicio3/ejercicio3/Program.cs
@@ -0,0 +1,33 @@
+﻿// 3. Agregar 3, eliminar 2 y mostrar estado.
+
+using System;
+using System.Collections.Generic;
+
+class Ejercicio3
+{
+    static void Main()
+    {
+        Stack<string> miPila = new Stack<string>();
+        miPila.Push("Elemento 1");
+        miPila.Push("Elemento 2");
+        miPila.Push("Elemento 3");
+        Console.WriteLine("Se agregaron 3 elementos.");
+        miPila.Pop(); 
+        miPila.Pop(); 
+        Console.WriteLine("Se eliminaron 2 elementos.");
+        Console.WriteLine("\n--- ESTADO FINAL DE LA PILA ---");
+
+        if (miPila.Count > 0)
+        {
+            foreach (var item in miPila)
+            {
+                Console.WriteLine("Queda en la pila: " + item);
+            }
+        }
+        else
+        {
+            Console.WriteLine("La pila está vacía.");
+        }
+        Console.ReadLine();
+    }
+}
